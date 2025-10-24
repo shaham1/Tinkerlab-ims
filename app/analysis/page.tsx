18a0e34bd73c8e814dcf08e2423c43f55cdb34c6
shaham1 <85864405+shaham1@users.noreply.github.com>
2025-10-24 17:52:21 +0500

    Delete app/analysis directory

--- app/analysis/page.tsx
+++
@@ -1,8 +0,0 @@
-/*
-get all transactions, make a report of:
-- items that are never/rarely used.
-- items that are commonly damaged.
-- items that need to be replaced.
-- items that are never 
-- 
-*/
